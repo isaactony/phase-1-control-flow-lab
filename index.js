854b5539acbc0b330bf0e744ea4a56cabb060d03
isaac <isaactonyloi@gmail.com>
2022-11-25 16:10:42 +0300

    control statements

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result;
+  if (distance <= 400) {
+    result =  "This one is on me!";
+    return result;
+  } else if (distance <= 2500 ) {
+    result =  "I will gladly take your thirty bucks.";
+    return result;
+  } else {
+    result =  "No can do.";
+    return result;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  let result = (destination=="NYC")?'Ok, sounds good.':'No go.';
+  return result;
+    
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let result;
+
+  switch(tip) {
+
+    case "generous":
+      result = "Thank you so much.";
+      return result;
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      return result;
+      break;
+      
+    default:
+      result = "Bye."
+      return result;
+  }
+  
+  }
+
